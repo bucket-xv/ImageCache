d971de5c77811d903ebb866520e7850cff49986c
Chenhao Xu <xv_chen_hao@163.com>
2025-03-17 16:39:32 +0800

    Fixing

--- create_files.sh
+++ create_files.sh
@@ -1,4 +1,4 @@
 mkdir -p data/app1/zip
 mkdir -p data/app2/zip
-dd if=/dev/urandom of=data/app1/zip/file.img bs=1M count=128
+dd if=/dev/urandom of=data/app1/zip/file.img bs=1M count=512
 dd if=/dev/urandom of=data/app2/zip/file2.img bs=1M count=1
