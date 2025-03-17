eb1337dead019831bc8d65298dcc3faba08e1d61
Chenhao Xu <xv_chen_hao@163.com>
2025-03-17 16:46:07 +0800

    Fixing

--- create_files.sh
+++ create_files.sh
@@ -1,4 +1,6 @@
 mkdir -p data/app1/zip
 mkdir -p data/app2/zip
-dd if=/dev/urandom of=data/app1/zip/file.img bs=1M count=512
-dd if=/dev/urandom of=data/app2/zip/file2.img bs=1M count=1
+mkdir -p data/app3/zip
+dd if=/dev/urandom of=data/app1/zip/file1.img bs=1M count=1024
+dd if=/dev/urandom of=data/app2/zip/file2.img bs=1M count=128
+dd if=/dev/urandom of=data/app3/zip/file3.img bs=1M count=1
--- test.py
+++ test.py
@@ -6,11 +6,14 @@ import argparse
 import os
 total_cache_miss = 0
 total_cache_miss_lock = threading.Lock()
+total_pulling_time = 0
+total_pulling_time_lock = threading.Lock()
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
 def thread_func(cache, folder_to_zip, image_name, container_name, iterations, verbose=False):
     cache_miss = 0
+    pulling_time = 0
     for i in range(iterations):
         print(f"Iteration {i+1} of {iterations} with image {image_name}")
         result = subprocess.run(f"docker images -q {image_name}", shell=True, capture_output=True, text=True)
@@ -18,7 +21,10 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
         # When the image is not in the cache, it will be pulled, and the cache will be updated
         cache.detect_run(image_name, container_name)
         if result.stdout.strip() == '':
+            start_time = time.time()
             subprocess.run(f"docker pull {image_name}", shell=True,capture_output=not verbose, text=True)
+            end_time = time.time()
+            pulling_time += end_time - start_time
             cache_miss += 1
         else:
             print(f"Image {image_name} already in cache")
@@ -35,6 +41,9 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
     with total_cache_miss_lock:
         global total_cache_miss
         total_cache_miss += cache_miss
+    with total_pulling_time_lock:
+        global total_pulling_time
+        total_pulling_time += pulling_time
 
 def main():
     parser = argparse.ArgumentParser()
@@ -43,31 +52,36 @@ def main():
     registry_ip = args.ip
 
     cache = DockerImageCache(time_window=60, cache_size=1, policy=EvictionPolicy.LEAST_FREQUENTLY_USED)
-    folder1_to_zip = os.path.join(project_dir, 'data/app1')  
-    folder2_to_zip = os.path.join(project_dir, 'data/app2')
-    iteration1 = 4
-    iteration2 = 8
-    total_iterations = iteration1 + iteration2
+    folders_to_zip = [
+        os.path.join(project_dir, 'data/app1'),
+        os.path.join(project_dir, 'data/app2'),
+        os.path.join(project_dir, 'data/app3')
+    ]
+    iterations = [4, 8, 16]
+    total_iterations = sum(iterations)
 
     # Create one thread for each function
-    thread1 = threading.Thread(target=thread_func, args=(cache,folder1_to_zip, f'{registry_ip}:5000/image-cache-app1:latest', 'container1', iteration1))
-    thread2 = threading.Thread(target=thread_func, args=(cache,folder2_to_zip, f'{registry_ip}:5000/image-cache-app2:latest', 'container2', iteration2))
+    threads = []
+    for i, folder in enumerate(folders_to_zip):
+        thread = threading.Thread(target=thread_func, args=(cache,folder, f'{registry_ip}:5000/image-cache-app{i+1}:latest', f'container{i+1}', iterations[i]))
+        threads.append(thread)
+
 
     # Start the threads
-    thread1.start()
-    thread2.start()
+    for thread in threads:
+        thread.start()
 
     # Wait for all threads to finish
-    thread1.join()
-    thread2.join()
+    for thread in threads:
+        thread.join()
 
     # Clean up the images
-    subprocess.run(f"docker rmi {registry_ip}:5000/image-cache-app1:latest", shell=True, text=True)
-    subprocess.run(f"docker rmi {registry_ip}:5000/image-cache-app2:latest", shell=True, text=True)
+    for i in range(len(folders_to_zip)):
+        subprocess.run(f"docker rmi {registry_ip}:5000/image-cache-app{i+1}:latest", shell=True, text=True)
 
     print("All threads have finished")
-    print(f"Total iterations: {total_iterations}")
-    print(f"Total cache miss: {total_cache_miss}")
+    print(f"Total cache miss / total iterations: {total_cache_miss} / {total_iterations}")
+    print(f"Total pulling time: {total_pulling_time} seconds")
 
 if __name__ == "__main__":
     main()
