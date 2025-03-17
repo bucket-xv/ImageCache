bdd1c4e7cecfca6d6414f618a17858dbc1d9cd8d
Chenhao Xu <xv_chen_hao@163.com>
2025-03-17 18:15:04 +0800

    Fixing

--- test.py
+++ test.py
@@ -15,7 +15,8 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
     cache_miss = 0
     pulling_time = 0
     for i in range(iterations):
-        print(f"Iteration {i+1} of {iterations} with image {image_name}")
+        if verbose:
+            print(f"Iteration {i+1} of {iterations} with image {image_name}")
         
         message = cache.put_image(image_name, container_name)
         # When the image is not in the cache, it will be pulled, and the cache will be updated
@@ -25,14 +26,16 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
             message = cache.put_image(image_name, container_name)
 
         if message == "Already in cache":
-            print(f"Image {image_name} cache hit")
+            if verbose:
+                print(f"Image {image_name} cache hit")
         elif message == "Directly put in cache":
             cache_miss += 1
             start_time = time.time()
             subprocess.run(f"docker pull {image_name}", shell=True,capture_output=not verbose, text=True)
             end_time = time.time()
             pulling_time += end_time - start_time
-            print(f"Cold miss so Image {image_name} pulled")
+            if verbose:
+                print(f"Cold miss so Image {image_name} pulled")
         else:
             image_to_evict = message
             print(f"Evicting image: {image_to_evict}")
@@ -42,7 +45,8 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
             subprocess.run(f"docker pull {image_name}", shell=True,capture_output=not verbose, text=True)
             end_time = time.time()
             pulling_time += end_time - start_time
-            print(f"Capcity miss so Image {image_name} pulled")
+            if verbose:
+                print(f"Capcity miss so Image {image_name} pulled")
         
         # Run the container
         subprocess.run(f"docker run -v {folder_to_zip}:/files --rm --name {container_name} {image_name}", shell=True, capture_output=not verbose, text=True)
@@ -59,39 +63,46 @@ def thread_func(cache, folder_to_zip, image_name, container_name, iterations, ve
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, required=True)
+    parser.add_argument("--ip", "-i", type=str, required=True)
+    parser.add_argument("--verbose", "-v", type=bool, required=False, default=False)
     args = parser.parse_args()
     registry_ip = args.ip
-
-    cache = DockerImageCache(time_window=60, cache_size=1, policy=EvictionPolicy.LEAST_FREQUENTLY_USED)
-    folders_to_zip = [
-        os.path.join(project_dir, f'data/app{i+1}/zip') for i in range(3)
-    ]
     iterations = [6, 10, 16]
     total_iterations = sum(iterations)
+    policies = [EvictionPolicy.LEAST_FREQUENTLY_USED, EvictionPolicy.LEAST_TOTAL_TIME_USED]
 
-    # Create one thread for each function
-    threads = []
-    for i, folder in enumerate(folders_to_zip):
-        thread = threading.Thread(target=thread_func, args=(cache,folder, f'{registry_ip}:5000/image-cache-app{i+1}:latest', f'container{i+1}', iterations[i]))
-        threads.append(thread)
+    # Run the experiment for each policy
+    start_time = time.time()
+    for policy in policies:
+        # Clean up the images
+        for i in range(len(folders_to_zip)):
+            subprocess.run(f"docker rmi {registry_ip}:5000/image-cache-app{i+1}:latest", shell=True, text=True)
 
+        # Initialize the cache
+        cache = DockerImageCache(time_window=60, cache_size=1, policy=policy)
+        folders_to_zip = [
+            os.path.join(project_dir, f'data/app{i+1}/zip') for i in range(3)
+        ]
+        # Create one thread for each function
+        threads = []
+        for i, folder in enumerate(folders_to_zip):
+            thread = threading.Thread(target=thread_func, args=(cache,folder, f'{registry_ip}:5000/image-cache-app{i+1}:latest', f'container{i+1}', iterations[i], args.verbose))
+            threads.append(thread)
 
-    # Start the threads
-    for thread in threads:
-        thread.start()
+        # Start the threads
+        for thread in threads:
+            thread.start()
 
-    # Wait for all threads to finish
-    for thread in threads:
-        thread.join()
+        # Wait for all threads to finish
+        for thread in threads:
+            thread.join()
 
-    # Clean up the images
-    for i in range(len(folders_to_zip)):
-        subprocess.run(f"docker rmi {registry_ip}:5000/image-cache-app{i+1}:latest", shell=True, text=True)
+        print(f"Experiment {policy} summary:")
+        print(f"Total cache miss / total iterations: {total_cache_miss} / {total_iterations}")
+        print(f"Total pulling time: {total_pulling_time} seconds")
 
-    print("All threads have finished")
-    print(f"Total cache miss / total iterations: {total_cache_miss} / {total_iterations}")
-    print(f"Total pulling time: {total_pulling_time} seconds")
+    end_time = time.time()
+    print(f"Total experiment time: {end_time - start_time} seconds")
 
 if __name__ == "__main__":
     main()
